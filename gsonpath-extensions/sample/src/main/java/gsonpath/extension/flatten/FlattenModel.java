2101c66291dc31630f5212169ba2acabe2a5cadb
Lachlan McKee <lachlandevelopment@gmail.com>
2018-12-25 17:31:03 +0000

    Foo2

--- gsonpath-extensions/sample/src/main/java/gsonpath/extension/flatten/FlattenModel.java
+++ gsonpath-extensions/sample/src/main/java/gsonpath/extension/flatten/FlattenModel.java
@@ -2,6 +2,7 @@ package gsonpath.extension.flatten;
 
 import gsonpath.AutoGsonAdapter;
 import gsonpath.extension.annotation.FlattenJson;
+import gsonpath.extension.def.intdef.IntDefExample;
 
 @AutoGsonAdapter
 interface FlattenModel {
